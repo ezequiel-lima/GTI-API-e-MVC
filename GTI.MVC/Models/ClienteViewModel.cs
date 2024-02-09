2c2f3837a1634529236a72f7e7ea39ddf9133e3d
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2024-02-09 04:07:53 -0300

    FEAT: adicionado dataannotations

--- GTI.MVC/Models/ClienteViewModel.cs
+++ GTI.MVC/Models/ClienteViewModel.cs
@@ -1,16 +1,30 @@
-﻿namespace GTI.MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GTI.MVC.Models
 {
     public class ClienteViewModel
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O CPF não pode ser nulo ou vazio.")]
+        [MinLength(11, ErrorMessage = "O CPF deve ter 11 caracteres.")]
+        [MaxLength(11, ErrorMessage = "O CPF deve ter 11 caracteres.")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "O CPF deve conter apenas números.")]
         public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "O nome não pode ser nulo ou vazio.")]
         public string Nome { get; set; }
-        public string Rg { get; set; }
-        public DateTime DataExpedicao { get; set; }
-        public string OrgaoExpedicao { get; set; }
-        public string Uf { get; set; }
+
+        public string? Rg { get; set; }
+        public DateTime? DataExpedicao { get; set; }
+        public string? OrgaoExpedicao { get; set; }
+        public string? Uf { get; set; }
         public DateTime DataDeNascimento { get; set; }
+
+        [Required(ErrorMessage = "O campo sexo é obrigatório.")]
         public string Sexo { get; set; }
+
+        [Required(ErrorMessage = "O estado civil é obrigatório.")]
         public string EstadoCivil { get; set; }
         public List<EnderecoViewModel> Enderecos { get; set; }
     }
--- GTI.MVC/Models/EnderecoViewModel.cs
+++ GTI.MVC/Models/EnderecoViewModel.cs
@@ -1,13 +1,30 @@
-﻿namespace GTI.MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GTI.MVC.Models
 {
     public class EnderecoViewModel
     {
+        [Required(ErrorMessage = "O CEP não pode ser nulo ou vazio.")]
+        [MinLength(8, ErrorMessage = "O CEP deve ter 8 caracteres.")]
+        [MaxLength(8, ErrorMessage = "O CEP deve ter 8 caracteres.")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "O CEP deve conter apenas números.")]
         public string Cep { get; set; }
+
+        [Required(ErrorMessage = "O Rua não pode ser nulo ou vazio.")]
         public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O Número não pode ser nulo ou vazio.")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "deve conter apenas números.")]
         public string Numero { get; set; }
         public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "O Bairro não pode ser nulo ou vazio.")]
         public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O Cidade não pode ser nulo ou vazio.")]
         public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O UF não pode ser nulo ou vazio.")]
         public string Uf { get; set; }
     }
 }
