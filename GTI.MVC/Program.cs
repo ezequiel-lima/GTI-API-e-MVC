2486edc4a0ef4f7d0960ec429a961277e23f3492
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2024-02-09 01:05:53 -0300

    STYLE: alterado visual e ajustado nomes das tabelas

--- GTI.MVC/Program.cs
+++ GTI.MVC/Program.cs
@@ -38,7 +38,7 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllers();
     endpoints.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
+        pattern: "{controller=Cliente}/{action=Index}/{id?}");
 });
 
 app.Run();
