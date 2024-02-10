f93dccd785680bab6575c647d1ded2ccc0f46720
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2024-02-10 00:42:47 -0300

    FEAT: adicionado enum para select de informacoes

---
+++ GTI.MVC/Enums/EOrgaoExpedicao.cs
@@ -0,0 +1,22 @@
+﻿namespace GTI.MVC.Enums
+{
+    public enum EOrgaoExpedicao
+    {
+        // Somente exemplos e não reflete a realidade
+        Detran,
+        PF,
+        INSS,
+        MRE,
+        IIRGD,
+        SSP,
+        COREN, 
+        CRM,   
+        OAB,   
+        CRB,   
+        CREA,  
+        CRF,   
+        CRECI, 
+        CRO,   
+        CRESS 
+    }
+}
